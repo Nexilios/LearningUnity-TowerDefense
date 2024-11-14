4ebeb84d9337e8e28be8b305f219fe8845c8ab4e
Nicholas Reynaldo <nexilios@gmail.com>
2024-11-14 19:19:09 +0700

    Optimized MoneyUI
    + Created a public event in PlayerStats called OnMoneyChanged.
    + Created a setter/getter function for Money variable in PlayerStats.
    + Created UpdateMoneyUI in MoneyUI.
    ~ Subscribed UpdateMoneyUI to OnMoneyChanged event.
    
    Summary: Now money text will only update whenever the property Money in PlayerStats update.

--- Assets/Scripts/MoneyUI.cs
+++ Assets/Scripts/MoneyUI.cs
@@ -5,7 +5,17 @@ public class MoneyUI : MonoBehaviour
 {
     public TextMeshProUGUI moneyText;
     
-    void Update()
+    void OnEnable()
+    {
+        PlayerStats.OnMoneyChanged += UpdateMoneyUI;
+    }
+
+    void OnDisable()
+    {
+        PlayerStats.OnMoneyChanged -= UpdateMoneyUI;
+    }
+    
+    void UpdateMoneyUI()
     {
         moneyText.text = "$" + PlayerStats.Money;
     }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,10 +1,26 @@
+using System;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public static int Money;
+    public static event Action OnMoneyChanged;
+    
     public int startMoney = 400;
+    
+    private static int _money;
+    public static int Money
+    {
+        get => _money;
+        set
+        {
+            if (_money == value) return;
+            
+            _money = value;
+            OnMoneyChanged?.Invoke();
+        }
+    }
 
+    
     void Start()
     {
         Money = startMoney;
